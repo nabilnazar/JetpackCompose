a006cc0d23bd5879740d3c68a56dd8365a8c2bf8
nabilnazarp <nabilnazarp@gmail.com>
2023-02-13 09:41:47 +0530

    working with botton

---
+++ app/src/main/java/com/meghamlabs/jetpackcompose/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.meghamlabs.jetpackcompose
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.meghamlabs.jetpackcompose.ui.theme.JetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+                    ButtonDemo()
+        }
+    }
+}
+
+@Composable
+fun ButtonDemo() {
+    Button(onClick = { }) {
+     Text(text = "add to cart")
+    }
+}
